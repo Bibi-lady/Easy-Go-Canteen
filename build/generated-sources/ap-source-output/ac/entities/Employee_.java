f02810747e320dd477d199c1764695fc2324ca3c
Bibi-lady <phileychauke@gmail.com>
2025-05-12 23:02:52 +0200

    EJBModule

---
+++ build/generated-sources/ap-source-output/ac/entities/Employee_.java
@@ -0,0 +1,17 @@
+package ac.entities;
+
+import ac.entities.User;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-11T09:36:38")
+@StaticMetamodel(Employee.class)
+public class Employee_ { 
+
+    public static volatile SingularAttribute<Employee, Long> id;
+    public static volatile SingularAttribute<Employee, String> department;
+    public static volatile SingularAttribute<Employee, Double> salary;
+    public static volatile SingularAttribute<Employee, User> users;
+
+}
---
+++ build/generated-sources/ap-source-output/ac/entities/User_.java
@@ -0,0 +1,16 @@
+package ac.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2025-05-11T09:36:38")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, String> role;
+    public static volatile SingularAttribute<User, Long> id;
+    public static volatile SingularAttribute<User, String> username;
+
+}
